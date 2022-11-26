7318553e9e145475ecc21856865a1d7af04a8238
Thamyris <thamyriscabral@hotmail.com>
2022-11-26 10:45:47 -0300

    Treinando consicionais no DOM

--- DOM/condicionais-DOM.js
+++ DOM/condicionais-DOM.js
@@ -2,7 +2,7 @@ let situacao1 = document.querySelector('#situacao1');
 let situacao2 = document.querySelector('#situacao2');
 let situacao3 = document.querySelector('#situacao3');
 
-let nota = 2;
+let nota = 9;
 
 if (nota >= 7) {
   situacao1.textContent = 'Aprovado';
@@ -10,4 +10,22 @@ if (nota >= 7) {
   situacao1.textContent = 'Recuperação';
 } else {
   situacao1.textContent = 'Reprovado';
+}
+
+nota >= 7
+  ? (situacao2.textContent = 'Aprovado')
+  : (situacao2.textContent = 'Recuperação');
+
+let nota2 = 'Recuperaçao';
+switch (nota2) {
+  case 'Aprovado':
+    situacao3.textContent = 'Voce foi aprovado';
+    break;
+
+  case 'Recuperaçao':
+    situacao3.textContent = 'Voce está em Recuperaçao';
+    break;
+
+  default:
+    situacao3.textContent = 'Voce foi reprovado';
 }
